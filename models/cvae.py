7f5c460085f796990dc1d58f14d7fd0087f6c085
Siming <simingrox@gmail.com>
2023-12-15 00:26:11 -0500

    model visualization

--- models/cvae.py
+++ models/cvae.py
@@ -2,10 +2,9 @@ import torch
 import torch.nn as nn
 
 class CVAE(nn.Module):
-    def __init__(self, input_dim, hidden_dim, latent_dim, num_classes):
+    def __init__(self, input_dim, hidden_dim, latent_dim):
         super(CVAE, self).__init__()
         self.latent_dim = latent_dim
-        self.num_classes = num_classes
 
         self.encoder = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
@@ -18,7 +17,7 @@ class CVAE(nn.Module):
         )
 
         self.decoder = nn.Sequential(
-            nn.Linear(latent_dim + num_classes, hidden_dim),
+            nn.Linear(latent_dim, hidden_dim),
             nn.Dropout(0.2),
             nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim),
@@ -44,7 +43,7 @@ class CVAE(nn.Module):
         decoded = self.decoder(z)
         return decoded
 
-    def forward(self, x, y):
+    def forward(self, x):
         mean, logvar = self.encode(x)
         z = self.reparameterize(mean, logvar)
         output = self.decode(z)
