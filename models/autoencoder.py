911f5e6aa95324a11fd2aed920d83d7697fc34d6
Siming <simingrox@gmail.com>
2023-12-14 22:04:57 -0500

    feat: autoencoder dropout to prevent overfitting

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -9,8 +9,10 @@ class Autoencoder(nn.Module):
         self.encoder = nn.Sequential(
             nn.Conv1d(1, 2, kernel_size=3, stride=2, padding=1),
             nn.AvgPool1d(kernel_size=2, stride=2, padding=0),
+            nn.Dropout1d(0.2),
             nn.ReLU(),
             nn.Conv1d(2, 4, kernel_size=3, stride=2, padding=1),
+            nn.Dropout1d(0.2),
             nn.ReLU(),
             nn.Conv1d(4, 1, kernel_size=3, padding=1),
             nn.ReLU()
@@ -19,8 +21,10 @@ class Autoencoder(nn.Module):
         # Decoder layers
         self.decoder = nn.Sequential(
             nn.ConvTranspose1d(1, 3, kernel_size=3, stride=2, padding=1),
+            nn.Dropout1d(0.2),
             nn.ReLU(),
             nn.ConvTranspose1d(3, 4, kernel_size=3, stride=2, padding=1),
+            nn.Dropout1d(0.2),
             nn.ReLU(),
             nn.ConvTranspose1d(4, 1, kernel_size=4, stride=2, padding=0),
             # nn.Sigmoid()
